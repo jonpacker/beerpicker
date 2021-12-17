5bc4e9d6e72bebcb32d257c3c29fae36355aab16
Jon Packer <contact@jonpacker.com>
2021-12-17 15:49:50 +0100

    init: beer picker

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
